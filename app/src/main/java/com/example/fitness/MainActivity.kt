03f885fd1b49cbad68383262468eb7c97cb55dd3
Bokjihee <bokjihee@naver.com>
2025-01-05 15:31:53 +0900

    [feat] logout connect

--- app/src/main/java/com/example/fitness/MainActivity.kt
+++ app/src/main/java/com/example/fitness/MainActivity.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.fitness.databinding.ActivityMainBinding
